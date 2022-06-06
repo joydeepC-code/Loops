3f3042213daebc200cbfdebe616ad2c2cbd5ac98
joydeepC-code <joydeepchanda1986@gmail.com>
2022-06-06 16:40:38 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# name = "abcdefg"
+
+# # for i in name:
+# #     print(i)
+
+# # print("---***---")
+
+# for j in range(4, 9+1):
+#     print(name)
+
+# a = int(input("Enter a number: "))
+
+# for t in range(1, 10+1):
+#     b = a * t
+#     print(a, "*", t,"=", b)
+
+a = int(input("Enter a number: "))
+
+t = 1
+while t <= 10:
+    b = a * t
+    print(a, "*", t, "=", b)
+    t += 1
